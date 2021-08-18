2e90bfa9551d1ce7c669448237eaa5734b6bd0fb
remu <986563206@qq.com>
2021-08-18 09:31:26 +0800

    Update README.md

--- src/main/java/top/tocome/io/File.java
+++ src/main/java/top/tocome/io/File.java
@@ -4,7 +4,12 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
 public class File {
-
+    /**
+     * 读取文件
+     *
+     * @param filePath 源文件路径
+     * @return 文件内容
+     */
     public static String read(String filePath) {
         try {
             return Stream.readString(new FileInputStream(filePath));
@@ -13,6 +18,15 @@ public class File {
         }
         return null;
     }
+
+    /**
+     * 写入文件
+     *
+     * @param filePath 源文件路径
+     * @param contents 写入的内容
+     * @param append   添加或覆盖
+     * @return 执行结果
+     */
 
     public static boolean write(String filePath, byte[] contents, boolean append) {
         try {
@@ -23,6 +37,14 @@ public class File {
         return false;
     }
 
+    /**
+     * 复制文件内容
+     *
+     * @param filePath 源文件路径
+     * @param savePath 保存的路径
+     * @param append   添加或覆盖
+     * @return 执行结果
+     */
     public static boolean copy(String filePath, String savePath, boolean append) {
         try {
             return Stream.copy(new FileInputStream(filePath), new FileOutputStream(savePath, append));
